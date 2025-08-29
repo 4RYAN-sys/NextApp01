2de7c93dd2391b362e7fdb6548ed5a2c159b9a29
4RYAN-sys <susantoaryan488@gmail.com>
2025-08-29 09:23:34 +0700

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen px-4 text-center bg-gradient-to-b from-blue-900 via-blue-950 to-blue-800 text-white">
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">Empowering Growth with Seamless Technical Solutions</h1>
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">Tumbuh lebih baik bersama kami!</h1>
       <p className="text-xl md:text-2xl max-w-2xl">
         Your Trusted Partner for Maximizing Impact and Results
       </p>
